801a752d84035cdcf191d1d48ba88fdaa5ec3e6e
afbelardi <afbelardi@gmail.com>
2023-01-17 15:27:13 -0500

    images added onto page and spaced

---
+++ src/assets/photoLinks.js
@@ -0,0 +1,36 @@
+
+
+const photoColumn1 = [
+    {
+        link: 'https://i.ibb.co/c6X8rHp/DSC03734.jpg'
+    },
+    {
+        link: 'https://i.ibb.co/L5gX6YV/DSC02967.jpg'
+    },
+    {
+        link: 'https://i.ibb.co/L5gX6YV/DSC02967.jpg'
+    },
+    {
+        link: 'https://i.ibb.co/Y2xTvPh/DSC03964.jpg'
+    }
+]
+
+const photoColumn2 = [
+    {
+        link: 'https://i.ibb.co/c6X8rHp/DSC03734.jpg'
+    },
+    {
+        link: 'https://i.ibb.co/L5gX6YV/DSC02967.jpg'
+    },
+    {
+        link: 'https://i.ibb.co/L5gX6YV/DSC02967.jpg'
+    },
+    {
+        link: 'https://i.ibb.co/Y2xTvPh/DSC03964.jpg'
+    }
+]
+
+module.exports = {
+    photoColumn1,
+    photoColumn2
+}
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { photoColumn1 } from '../assets/photoLinks';
+import { photoColumn2 } from '../assets/photoLinks';
 
 export default function ImageSlider (props) {
+ 
+    
     return(
         <div className="image-wrapper">
-            <div id="image-column-1"></div>
-            <div id="image-column-2"></div>
+            <div id="image-column-1">
+            {Object.keys(photoColumn1).length
+            ? photoColumn1.map(link => {
+                return (
+                    <img className="images" src={link.link}></img>
+                )
+            })
+        : ""}
+            </div>
+            <div id="image-column-2">
+            {Object.keys(photoColumn2).length
+            ? photoColumn2.map(link => {
+                return (
+                    <img className="images" src={link.link}></img>
+                )
+            })
+        : ""}
+            </div>
         </div>
     )
 }
