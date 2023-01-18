f49f7ff18d0491c409c81085e8740fdab0019b8f
afbelardi <afbelardi@gmail.com>
2023-01-18 13:24:00 -0500

    restructured image container

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -7,25 +7,9 @@ export default function ImageSlider (props) {
  
     
     return(
-        <div className="image-wrapper">
-            <div id="image-column-1">
-            {Object.keys(photoColumn1).length
-            ? photoColumn1.map(link => {
-                return (
-                    <img className="images" src={link.link}></img>
-                )
-            })
-        : ""}
-            </div>
-            <div id="image-column-2">
-            {Object.keys(photoColumn2).length
-            ? photoColumn2.map(link => {
-                return (
-                    <img className="images" src={link.link}></img>
-                )
-            })
-        : ""}
-            </div>
+        <div id="animation-wrapper">
+        <div id="image-wrapper">
+        </div>
         </div>
     )
 }
