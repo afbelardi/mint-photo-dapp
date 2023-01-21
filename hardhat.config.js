107cbfcda8b2476dea1e493711b6adaa708a8e80
afbelardi <afbelardi@gmail.com>
2023-01-21 18:39:57 -0500

    hardhat config set up

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,4 +7,13 @@ dotenv.config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: process.env.REACT_APP_URL,
+      accounts: [process.env.REACT_APP_PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.REACT_APP_ETHERSCAN_KEY
+  }
 };
