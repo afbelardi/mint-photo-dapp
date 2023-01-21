8635f4529bf6c91346491be84371f6774c5be604
afbelardi <afbelardi@gmail.com>
2023-01-21 18:56:37 -0500

    deploy script set up

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -13,13 +13,13 @@ async function main() {
 
   const lockedAmount = hre.ethers.utils.parseEther("1");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const AfbelardiPhotoNFT = await hre.ethers.getContractFactory("AfbelardiPhotoNFT");
+  const afbelardiPhotoNFT = await AfbelardiPhotoNFT.deploy();
 
-  await lock.deployed();
+  await afbelardiPhotoNFT.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+   "Afbelardi Photo NFT deployed to:", afbelardiPhotoNFT.address
   );
 }
 
