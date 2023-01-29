a6a0c7a75cc215abb86ea8744475af5d429fbec7
afbelardi <afbelardi@gmail.com>
2023-01-29 11:25:48 -0500

    added more functions to smart contract

--- contracts/AfbelardiPhotoNFT.sol
+++ contracts/AfbelardiPhotoNFT.sol
@@ -12,8 +12,49 @@ contract AfbelardiPhotoNFT is ERC721, Ownable {
     uint256 public totalSupply;
     uint256 public maxSupply;
     uint public maxPerWallet;
-    bool public isPublicMintEnabledl
+    bool public isPublicMintEnabled;
     string internal baseTokenUri;
     address payable public withdrawWallet;
     mapping(address => uint256) public walletMints;
+
+
+    constructor() payable ERC721('AfbelardiPhotoNFT', 'AFB') {
+        mintPrice = 0.02 ether;
+        totalSupply = 0;
+        maxSupply = 9;
+        maxPerWallet = 3;
+        withdrawWallet = 0xf72cb599DeB8f2cB5768B6Cd94a295C299Bd612F;
+    }
+
+    function setIsPublicMintEnabled(bool isPublicMintEnabled_) external onlyOwner {
+        isPublicMintEnabled = isPublicMintEnabled_;
+    }
+
+    function setBaseTokenUri(string calldata baseTokenUri_) external onlyOwner {
+        baseTokenUri = baseTokenUri_;
+    }
+
+    function tokenURI(uint256 tokenId_) public view override returns (string memory) {
+        require(_exists(tokenId_), 'Token does not exist!');
+        return string(abi.encodePacked(baseTokenUri, Strings.toString(tokenId_, ".json"));
+    }
+
+    function withdraw() external onlyOwner {
+        (bool success, ) = withdrawWallet.call{ value: address(this).balance}('');
+        require(success, 'withdraw failed');
+    }
+
+    function mint(uint256 quantity_) public payable {
+        require(isPublicMintEnabled, 'Minting not enabled');
+        require(msg.value == quantity_ * mintPrice, 'Wrong mint value');
+        require(totalSupply + quantity_ <= maxSupply, 'Sold out');
+        require(walletMints[msg.sender] + quantity_ <= maxPerWallet, 'Exceeded max wallet');
+
+        for (uint256 i = 0; i < quantity_; i++) {
+            uint256 newTokenId = totalSupply + 1;
+            totalSupply++;
+            _safeMint(msg.sender, newTokenId);
+        }
+    }
+
 }
